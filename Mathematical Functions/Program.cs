63badc91b5a0fa7ff6c6288211b7a6d9af294ad3
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-01-15 10:10:06 -0500

    Create Mathematical Functions project and add basic template to program.cs

---
+++ Mathematical Functions/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematical_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 1
+            // Partially worked example
+            Console.WriteLine("\nPart 1, circumference and area of a circle.");
+            Console.Write("Enter an integer for the radius: ");
+            string strradius = Console.ReadLine();
+            int intradius = int.Parse(strradius);
+            double circumference = 2 * Math.PI * intradius;
+            Console.WriteLine($"The circumference is {circumference}");
+
+            // Implementation for area here
+
+            Console.WriteLine($"The area is {area}");
+
+
+            // Part 2
+            Console.WriteLine("\nPart 2, volume of a hemisphere.");
+
+            // Implementation here
+
+            Console.WriteLine($"The volume is {volume}");
+
+
+            // Part 3
+            Console.WriteLine("\nPart 3, area of a triangle (Heron's formula).");
+
+            // Implementation here
+
+            Console.WriteLine($"The area is {area}");
+
+
+            // Part 4
+            Console.WriteLine("\nPart 4, solving a quadratic equation.");
+
+            // Implementation here
+
+            Console.WriteLine($"The positive solution is {positive_num / denominator}");
+            Console.WriteLine($"The negative solution is {negative_num / denominator}");
+        }
+    }
+}
